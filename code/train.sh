1335cc05758be1612412484952df80f5c842f748
destinyls <1491078277@qq.com>
2021-12-23 21:18:22 +0800

    GUPNet Baseline

---
+++ code/train.sh
@@ -0,0 +1,8 @@
+python tools/train_val.py --config experiments/config.yaml
+
+python tools/train_val.py --config experiments/config_002.yaml
+
+python tools/train_val.py --config experiments/config_003.yaml
+
+python tools/train_val.py --config experiments/config_004.yaml
+
